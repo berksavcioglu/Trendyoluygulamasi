c7f840aef7646d41937bd271f82020d84fcdfce2
berksavcioglu <savciogluberk@gmail.com>
2022-05-07 16:40:35 +0300

    Tab itemların sayfalarını ayrı ayrı boş şekilde oluşturdum

---
+++ Trendyoluygulamasi/Trendyoluygulamasi/Views/Yemeksayfasi.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trendyoluygulamasi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Yemeksayfasi : ContentPage
+    {
+        public Yemeksayfasi()
+        {
+            InitializeComponent();
+        }
+    }
+}
